b191209f67ab7e77823bc1d679a113198d730ad6
Piterxus <piterxus@gmail.com>
2025-06-12 19:05:42 +0200

    fix(cdk): move resource lookups within a valid stack

--- infra/lib/lambda-stack.ts
+++ infra/lib/lambda-stack.ts
@@ -1,42 +1,47 @@
-// lambda-stack.ts
-import * as cdk from 'aws-cdk-lib';
+import { Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import * as lambda from 'aws-cdk-lib/aws-lambda';
-import * as apigw from 'aws-cdk-lib/aws-apigateway';
+import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs';
+import * as path from 'path';
+import * as apigateway from 'aws-cdk-lib/aws-apigateway';
 import * as ec2 from 'aws-cdk-lib/aws-ec2';
 import * as dynamodb from 'aws-cdk-lib/aws-dynamodb';
+import * as cdk from 'aws-cdk-lib';
 
-interface LambdaStackProps extends cdk.StackProps {
+export interface LambdaStackProps extends cdk.StackProps {
     vpc: ec2.IVpc;
     table: dynamodb.ITable;
     securityGroup: ec2.ISecurityGroup;
     instanceId: string;
-}
-
-export class LambdaStack extends cdk.Stack {
-    constructor(scope: Construct, id: string, props: LambdaStackProps) {
+  }
+export class LambdaStack extends Stack {
+    constructor(scope: Construct, id: string, props?: LambdaStackProps) {
         super(scope, id, props);
 
-        const lambdaFn = new lambda.Function(this, 'InfraLambda', {
+        if (!props) {
+            throw new Error('Props are required for LambdaStack');
+        }
+
+        const challengeLambda = new NodejsFunction(this, 'ChallengeLambda', {
+            entry: path.join(__dirname, '../lambda/index.js'), // tu Lambda en JS
+            handler: 'handler',
             runtime: lambda.Runtime.NODEJS_20_X,
-            handler: 'index.handler',
-            code: lambda.Code.fromAsset('lambda'),
             vpc: props.vpc,
-            vpcSubnets: { subnetType: ec2.SubnetType.PRIVATE_WITH_EGRESS },
             securityGroups: [props.securityGroup],
-            environment: {
-                EC2_INSTANCE_ID: props.instanceId,
-                DDB_TABLE_NAME: props.table.tableName,
+            bundling: {
+                externalModules: ['aws-sdk'], // no empacarla, ya está en el entorno Lambda
             },
-            timeout: cdk.Duration.seconds(30),
+            environment: {
+                INSTANCE_ID: 'i-080aa23d1ecd83299',
+                TABLE_NAME: 'Challenges',
+            },
         });
 
-        const api = new apigw.LambdaRestApi(this, 'InfraApi', {
-            handler: lambdaFn,
-            proxy: false,
+        const api = new apigateway.RestApi(this, 'ChallengeApi', {
+            restApiName: 'Challenge Generator',
         });
 
         const challenges = api.root.addResource('challenges');
-        challenges.addMethod('POST');
+        challenges.addMethod('POST', new apigateway.LambdaIntegration(challengeLambda));
     }
 }
